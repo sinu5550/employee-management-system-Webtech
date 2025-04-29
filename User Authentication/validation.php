f64f50e6a24ebe45972715bf606bc93143e67f61
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2025-04-29 14:09:17 +0600

    Reg validation alo completed with php

--- User Authentication/validation.php
+++ User Authentication/validation.php
@@ -1,18 +1,25 @@
 <?php
+
+$name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $password = $_REQUEST['password'];
+$confirmPassword = $_REQUEST['confirmPassword'];
+
+
 $atIndex = strpos($email, "@");
 $dotIndex = strpos($email, ".");
 
-if ($email == "" || $password == "") {
-    echo "<p style='color:red; font-size:24px;'>Null data found!</p>";
+if ($email == "" || $password == "" || $confirmPassword == "" || $name == "") {
+    echo "<p style='color:red; font-size:24px;'>Input field can not be Empty!</p>";
 } else if ($atIndex < 1 || $dotIndex < $atIndex + 2 || $dotIndex + 2 >= strlen($email)) {
     echo "<p style='color:red; font-size:24px;'>Enter a valid email address (e.g., anything@example.com).</p>";
 
 } else if (strlen($password) < 8) {
     echo "<p style='color:red; font-size:24px;'>Password must be at least 8 characters long.</p>";
+} else if ($password !== $confirmPassword) {
+    echo "<p style='color:red; font-size:24px;'>Password do not matched.</p>";
 } else {
-    echo "<p style='color:green; font-size:24px;'>The user is Valid.</p>";
+    echo "<p style='color:green; font-size:24px;'>Success.</p>";
 }
 
 
